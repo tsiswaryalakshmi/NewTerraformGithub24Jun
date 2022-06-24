050de3887f8795e124015ad47c6951a11b1b9694
tsiswaryalakshmi <99864357+tsiswaryalakshmi@users.noreply.github.com>
2022-06-24 17:00:31 +0530

    Create main.tf

---
+++ modules/subnet/main.tf
@@ -0,0 +1,6 @@
+resource "azurerm_subnet" "vm_subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.virtual_network_name
+  address_prefixes     = ["10.0.2.0/24"]
+}
