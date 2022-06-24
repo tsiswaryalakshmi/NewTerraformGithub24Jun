09dc4808ee10119a913b75a0da74f917224d40a6
tsiswaryalakshmi <99864357+tsiswaryalakshmi@users.noreply.github.com>
2022-06-24 17:01:49 +0530

    Add files via upload

---
+++ modules/virtualmachine/variables.tf
@@ -0,0 +1,59 @@
+variable "windows_virtual_machine" {
+    description = "(optional) describe your variable"
+}
+
+variable "resource_group_name" {
+    description = "(optional) describe your variable"
+}
+
+variable "resource_group_location" {
+    description = "(optional) describe your variable"
+}
+
+variable "publisher" {
+    description = "(optional) describe your variable"
+}
+
+variable "offer" {
+    description = "(optional) describe your variable"
+}
+
+variable "sku" {
+    description = "(optional) describe your variable"
+}
+
+variable "admin_username" {
+    description = "(optional) describe your variable"
+}
+
+variable "admin_password" {
+    description = "(optional) describe your variable"
+}
+
+variable "network_interface_ids" {
+    description = "(optional) describe your variable"
+}
+
+/*
+variable "virtual_network_name" {
+    description = "(optional) describe your variable"
+}
+
+
+variable "subnet_name" {
+    description = "(optional) describe your variable"
+}
+
+variable "address_space" {
+    description = "(optional) describe your variable"
+}
+
+variable "address_prefixes" {
+    description = "(optional) describe your variable"
+}
+
+variable "network_interface_name" {
+    description = "(optional) describe your variable"
+}
+
+*/
