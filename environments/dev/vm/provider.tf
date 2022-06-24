d52b34d67ac0fbab71ed6d0ee2cc6fc9154e14af
tsiswaryalakshmi <99864357+tsiswaryalakshmi@users.noreply.github.com>
2022-06-24 19:10:06 +0530

    Update provider.tf

--- environments/dev/vm/provider.tf
+++ environments/dev/vm/provider.tf
@@ -1,9 +1,22 @@
 terraform {
-  required_providers {
-    azurerm = ">=2.52.0"
-  }
+
+required_providers {
+
+azurerm = {
+
+source = "hashicorp/azurerm"
+
+version = ">=2.37.0"
+
 }
 
+}
+
+}
+
+# Configure the Microsoft Azure Provider
+
 provider "azurerm" {
-  features {}
+
+features {}
 }
