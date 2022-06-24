e17287c1fab6382508c380141114d001be7ff3ff
tsiswaryalakshmi <99864357+tsiswaryalakshmi@users.noreply.github.com>
2022-06-24 16:59:29 +0530

    Add files via upload

---
+++ modules/nic/output.tf
@@ -0,0 +1,7 @@
+output "nic_id" {
+    value = azurerm_network_interface.vm_nic.id
+}
+
+output "nic_name" {
+    value = azurerm_network_interface.vm_nic.name
+}
---
+++ modules/nic/variables.tf
@@ -0,0 +1,19 @@
+variable "network_interface_name" {
+    description = "(optional) describe your variable"
+}
+
+variable "resource_group_location" {
+    description = "(optional) describe your variable"
+}
+
+variable "resource_group_name" {
+    description = "(optional) describe your variable"
+}
+
+variable "subnet_id" {
+    description = "(optional) describe your variable"
+}
+
+variable "public_ip_address_id" {
+    description = "(optional) describe your variable"
+}
