a11b82384c7bda05949a47a53319c4ea66fa7726
tsiswaryalakshmi <99864357+tsiswaryalakshmi@users.noreply.github.com>
2022-06-24 17:01:11 +0530

    Add files via upload

---
+++ modules/subnet/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+    description = "(optional) describe your variable"
+}
+
+variable "virtual_network_name" {
+    description = "(optional) describe your variable"
+}
+
+variable "subnet_name" {
+    description = "(optional) describe your variable"
+}
