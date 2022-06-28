b74cf622fdd18bc2f94d61b5cd245a4e243ba152
tsiswaryalakshmi <99864357+tsiswaryalakshmi@users.noreply.github.com>
2022-06-28 17:18:54 +0530

    Update provider.tf

--- environments/dev/vm/provider.tf
+++ environments/dev/vm/provider.tf
@@ -8,11 +8,11 @@ provider "azurerm" {
   features {}
 }
 
-data "terraform_remote_state" "terraform_github" {
-  backend "azurerm" 
-    config  {
+terraform  {
+  backend "azurerm"  {
+    resource_group_name  ="TerraformDemo"
     storage_account_name = "terraformcodeops"
     container_name       = "tfstatefile"
     key                  = "dev.terraform.tfstate"
-   
+  }
 }
