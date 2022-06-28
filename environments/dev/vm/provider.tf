12c7cf3aed1498f53d11f3de9bbbd1241e214b75
tsiswaryalakshmi <99864357+tsiswaryalakshmi@users.noreply.github.com>
2022-06-28 17:28:58 +0530

    Update provider.tf

--- environments/dev/vm/provider.tf
+++ environments/dev/vm/provider.tf
@@ -13,6 +13,7 @@ terraform {
     resource_group_name  = "TerraformDemo"
     storage_account_name = "terraformcodeops"
     container_name       = "tfstatefile"
+    access_key           = "anMMpoLzekIyS5XZsI+TmMY71eY1NqRjJOf39GynBk+w5bekWYCLA0DDX+Tye40RhcjQfqDqMWiR+AStZkjLcw=="
     key                  = "dev.terraform.tfstate"
   }
 }
