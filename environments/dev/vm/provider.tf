8d036b714ceb75730064ed76cded244f8801ebc8
tsiswaryalakshmi <99864357+tsiswaryalakshmi@users.noreply.github.com>
2022-06-30 14:06:16 +0530

    Update provider.tf

--- environments/dev/vm/provider.tf
+++ environments/dev/vm/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = ">=2.52.0"
   }
-  backend "s3" {
+  backend "azurerm" {
     resource_group_name  = "tfstate"
     storage_account_name = "tfstate186305558"
     container_name       = "tfstate"
