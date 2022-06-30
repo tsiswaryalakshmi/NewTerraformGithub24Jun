75c04b6f775cbe9d15f9b50b6c388cf04f5f8af0
tsiswaryalakshmi <99864357+tsiswaryalakshmi@users.noreply.github.com>
2022-06-30 12:38:45 +0530

    Update provider.tf

--- environments/dev/vm/provider.tf
+++ environments/dev/vm/provider.tf
@@ -10,10 +10,10 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = ${{ secrets.AZURE_RESOURCE_GROUP }}
-    storage_account_name = ${{ secrets.AZURE_STORAGE_ACCOUNT }}
-    container_name       = ${{ secrets.AZURE_CONTAINER_NAME }}
-    key                  = ${{ secrets.AZURE_KEY }}
+    resource_group_name  = var.resource_group_name
+    storage_account_name = var.storage_account_name
+    container_name       = var.container_name
+    key                  = var.key
   }
 }
 
